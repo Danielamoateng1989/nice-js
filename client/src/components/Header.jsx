ddc13464683abeb0fd0d9979704a64eb152f7154
Daniel Amoateng <danielamoateng81@yahoo.com>
2022-02-15 13:14:34 -0500

    import links from navItems

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
+import {links} from '../navItems'
 
 const Header = () => {
+
+    
+    
   return (
+
+    <React.Fragment>
     <nav>
 
     
 
     </nav>
+    </React.Fragment>
   )
 }
 
