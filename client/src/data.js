ac9c0e51360c8d077b072af3b7f1ef440860b257
Daniel Amoateng <danielamoateng81@yahoo.com>
2022-02-15 18:30:53 -0500

    add beauty services to src folder

---
+++ client/src/data.js
@@ -0,0 +1,121 @@
+const beautyservices = [
+  {
+    name: 'Conrow',
+    image: '/images/braids-1.jpg',
+    description:
+      'A haircut that looks so good, you will like to do it every day. This looks come at cheap too',
+    brand: 'Braid',
+    price: 80.99,
+    rating: 4.5,
+    numberOfReviews: 2,
+  },
+  {
+    name: 'Young men haircut',
+    image: '/client/src/images/haircut-1.jpg',
+    description:
+      'This look is suited for young men with style. Get if for your boy',
+    brand: 'Haircut',
+    price: 30.55,
+    rating: 4.0,
+    numberOfReviews: 8,
+  },
+  {
+ 
+   name: 'Beautiful makeup',
+    image: '/client/src/images/makeup-2.jpg',
+    description:
+      'A very nice makeup. You can rock this to any occasion.',
+    brand: 'Haircut',
+    price: 40.00,
+    rating: 4.0,
+    numberOfReviews: 2,
+  },
+  {
+    name: 'Nice nails',
+    image: '/client/src/images/nail-1.jpg',
+    description:
+      'This is a very nice nail. It is very cheap too',
+    brand: 'Haircut',
+    price: 25.00,
+    rating: 4.5,
+    numberOfReviews: 8,
+  },
+  {
+    name: 'Men hair braid',
+    image: 'client/src/images/man-braids-1.jpg',
+    description:
+      'This is mens haircut. It is very nice to rock',
+    brand: 'Haircut',
+    price: 50.00,
+    rating: 4.0,
+    numberOfReviews: 8,
+  },
+  {
+
+    name: 'Make up with red lipstick',
+    image: 'client/src/images/makeup-2.jpg',
+    description:
+      'This is a very nice makeup and with red lipstick',
+    brand: 'Haircut',
+    price: 55.00,
+    rating: 4.5,
+    numberOfReviews: 4,
+  },
+  {
+
+    name: 'White colored nails',
+    image: 'client/src/images/nail-3.jpg',
+    description:
+      'This is a really nice nail color. It looks amazingly nice',
+    brand: 'Haircut',
+    price: 65.00,
+    rating: 4.0,
+    numberOfReviews: 8,
+  },
+  {
+
+    name: 'Mens haircut one',
+    image: 'client/src/images/haircut2.jpg',
+    description:
+      'This is a classic haircut that will make you look great.',
+    brand: 'Haircut',
+    price: 55.00,
+    rating: 4.0,
+    numberOfReviews: 8,
+  },
+  {
+
+    name: 'Colored nails for women',
+    image: 'client/src/images/nail-2.jpg',
+    description:
+      'This is a rainbow nails for women. It looks really neat. ',
+    brand: 'Haircut',
+    price: 35.00,
+    rating: 4.0,
+    numberOfReviews: 51,
+  },
+  {
+
+    name: 'black and brown nails',
+    image: 'client/src/images/nail-1.jpg',
+    description:
+      'This is a black and brown nails. We can come over to your hour and get you this service',
+    brand: 'Nails',
+    price: 60.00,
+    rating: 4.0,
+    numberOfReviews: 5,
+  },
+  {
+
+    name: 'Long hair for women',
+    image: 'client/src/images/womanhair-1.jpg',
+    description:
+      'This is a very nice haircut for women and the price is cheap too',
+    brand: 'Haircut',
+    price: 80.00,
+    rating: 4.0,
+    numberOfReviews: 3,
+  },
+]
+
+export default  beautyservices
