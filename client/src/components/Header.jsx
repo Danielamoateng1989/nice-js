61b7c56cacef9f2c2819bb851265575576fccfd0
Daniel Amoateng <danielamoateng81@yahoo.com>
2022-02-15 13:28:05 -0500

    1. react add-icons 2. add FaBars for toggling on small screens 3. listed the links using map 4. add social media links

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,19 +1,55 @@
 import React from 'react'
+import {FaBars, FaTwitter, FaGithub, FaLinkedin} from 'react-icons/fa'
 import {links} from '../navItems'
 
 const Header = () => {
-
-    
-    
+   
   return (
-
-    <React.Fragment>
     <nav>
+        <div className="nav-container">
+            <div className="nav-header">
+                <img src={} alt="Nice company logo" />
+                <button className="toggle">
+                   <FaBars />
+                </button>
+            </div>
+        </div>
+        
+        <div className="links-container">
+        <ul className="links">
 
-    
-
+            {links.map(link => {
+                const {id, text, url} = link
+                return (
+                    <li key={id}>
+                    <a href={url}>
+                    {text}
+                    </a>
+                   </li>
+                    )
+            })}
+        </ul>
+       </div>
+      
+       <ul className="social-icons">
+          <li>
+            <a href="https://twitter.com/danny_bee3">
+             <FaTwitter />
+            </a>
+          </li>
+          <li>
+            <a href="https://www.linkedin.com/in/daniel-amoateng-959a27181/">
+             <FaLinkedin />
+            </a>
+          </li>
+          <li>
+            <a href="https://github.com/Danielamoateng1989">
+              <FaGithub />
+            </a>
+          </li>
+        </ul>
     </nav>
-    </React.Fragment>
+    
   )
 }
 
