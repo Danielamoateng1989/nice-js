8dffe8da4590994cdba09f1cc9084dac4d69ecfc
Daniel Amoateng <danielamoateng81@yahoo.com>
2022-02-15 13:29:16 -0500

    chore: clean up

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -26,7 +26,7 @@ const Header = () => {
                     {text}
                     </a>
                    </li>
-                    )
+                )
             })}
         </ul>
        </div>
