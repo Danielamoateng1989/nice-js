2e30f54a98b0b83e7e6c0fab010f997fa4a532a4
Daniel Amoateng <danielamoateng81@yahoo.com>
2022-02-16 11:58:05 -0500

    add nice logo

--- client/src/components/Footer/Logo.jsx
+++ client/src/components/Footer/Logo.jsx
@@ -1,20 +1,13 @@
-import { chakra, useColorModeValue, useToken } from '@chakra-ui/react'
+
 import * as React from 'react'
 import logo from '../../images/nice.png'
 
-export const Logo = (props) => {
-  const [white, black] = useToken('colors', ['white', 'gray.800'])
+export const Logo = () => {
+ 
   return (
-    <chakra.svg
-      color={useColorModeValue('blue.500', 'blue.300')}
-      aria-hidden
-      viewBox="0 0 123 24"
-      fill="none"
-      h="6"
-      flexShrink={0}
-      {...props}
-    >
-      <img src={logo} alt="logo" />
-    </chakra.svg>
+    <>
+    <img src={logo} alt="nice compnay logo" className="nice-logo"/>
+    </>
+  
   )
 }
--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -7,15 +7,14 @@ import styled from 'styled-components'
 const Button = styled.button`
   font-size: 1em;
   margin: 1em;
-  border-radius: 50px;
+  border-radius: 30px;
   color: white;
   background-color: #E41E53;
   font-weight: bold;
-  height: 38px;
+  height: 30px;
   width: 90px;
     
 `
-
 const Navbar = () => {
 const [showLinks, setShowLinks] = useState(false)
 return (
