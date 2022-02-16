a2819a993c720dfcb93b53c8da2bfc9c3b2e8e0b
Daniel Amoateng <danielamoateng81@yahoo.com>
2022-02-16 17:55:22 -0500

    import uselocation to get the path of the service

--- client/src/pages/Servicespage.jsx
+++ client/src/pages/Servicespage.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import data from '../data'
+import {useLocation} from 'react-router-dom'
 
 
 
 const Servicespage = () => {
 
+  const location = useLocation()
+
+  console.log(location)
+
   return (
     <React.Fragment>
 
-     
+      <h1>Service Details</h1>
 
     </React.Fragment>
   )
