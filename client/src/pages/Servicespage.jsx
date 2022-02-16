434e5c5e3691b961c0af50ebfee3d59a2dab0fc1
Daniel Amoateng <danielamoateng81@yahoo.com>
2022-02-16 17:51:16 -0500

    remove product detail folder since we won't need it

--- client/src/pages/Servicespage.jsx
+++ client/src/pages/Servicespage.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
-import ProductDetails from '../components/ProductDetails/ProductDetails'
 import data from '../data'
 
 
 
 const Servicespage = () => {
 
-  
   return (
     <React.Fragment>
 
