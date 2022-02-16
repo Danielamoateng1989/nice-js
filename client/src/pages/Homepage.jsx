6f111f136c7aa103d46b83f052769124e4f9e131
Daniel Amoateng <danielamoateng81@yahoo.com>
2022-02-15 23:02:11 -0500

    add hero

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -1,28 +1,18 @@
 import React from 'react'
 import {Row, Col} from 'react-bootstrap'
+import { Hero } from '../components/hero'
 import Services from '../components/Service'
 import data from '../data'
 
 
 
+
 const Homepage = () => {
   return (
-   
-    <React.Fragment>
-      <Row>
-        {data.map((service, id) => {
-          console.log(service)
-          return (
-            <Col sm={12} md={6} lg={4} xl={3} key={id}>
-              <Services service={service} />
-            </Col>
-          )
-        })}
-      </Row>
-    
-    
-    
-    </React.Fragment>
+    <>
+     <Hero />
+    <h1>Home</h1>
+    </>
   )
 }
 
