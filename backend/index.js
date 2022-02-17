98cc4879f0212e31aebfb9c504aa4c6dcd1394f7
Daniel Amoateng <danielamoateng81@yahoo.com>
2022-02-17 08:21:52 -0500

    add a get request method to get a single product

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,25 @@
 import express from 'express';
 const app = express();
+import products from './data/data.js'
 
 
+app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello')
+//Fetching all services
+app.get('/api/services', (req, res) => {
+     res.send(products)
+     
+})
+
+//Fetching a single service
+app.get('/api/services/:id', (req, res) => {
+
+ const product = products.find(p => p.id === Number(req.params.id))
+   res.send(product)
 })
 
 
-
-
-
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 app.listen(PORT, () => {
  console.log(`server listening in port ${PORT}`)
 })
