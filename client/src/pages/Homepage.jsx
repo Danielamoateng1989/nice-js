9f528ed3e35ce188c530109d94a40b6f7b83bca3
Daniel Amoateng <danielamoateng81@yahoo.com>
2022-02-17 08:50:39 -0500

    chore: clean up

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -1,13 +1,9 @@
 import React from 'react'
 import { Hero } from '../components/Hero/Hero'
 import Products from '../components/Products/Products'
-import axios from 'axios'
+
 
 const Homepage = ({product}) => {
-
-
-  
-
   return (
     <>
      <Hero />
