b65f7e57dc0c48636f6c8cda5014c2ce3c3ce6a0
Daniel Amoateng <danielamoateng81@yahoo.com>
2022-02-19 06:10:41 -0500

    create date and time picker page

--- src/components/DateTimePicker/dateTimePicker.jsx
+++ src/components/DateTimePicker/dateTimePicker.jsx
@@ -1,21 +1,8 @@
-// or @mui/lab/Adapter{Dayjs,Luxon,Moment} or any valid date-io adapter
-import AdapterDateFns from '@mui/lab/AdapterDateFns';
-import LocalizationProvider from '@mui/lab/LocalizationProvider';
-import DateTimePicker from '@mui/lab/DateTimePicker'
 
-const PickDateAndTime = () => {
+
+export default function BasicDateTimePicker() {
+
   return (
-   <LocalizationProvider dateAdapter={AdapterDateFns}>
-  <DateTimePicker
-    renderInput={(props) => <TextField {...props} />}
-    label="DateTimePicker"
-    value={value}
-    onChange={(newValue) => {
-      setValue(newValue);
-    }}
-  />
-     </LocalizationProvider> 
+   <h1>Data and Time Picker here</h1>
   );
 }
-
-export default PickDateAndTime;
